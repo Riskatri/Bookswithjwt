2053f522c83717b8eafe1b4d95f7da82d204e6d0
Riska <handayaniriska762@gmail.com>
2020-02-17 22:13:55 +0700

    tambah validasi di signup

--- controller/authController.js
+++ controller/authController.js
@@ -4,28 +4,52 @@ const User = db.user;
 const Role = db.role;
 const Book = db.book;
 const asyncMiddleware = require("express-async-handler");
+const { validationResult } = require("express-validator/check");
+const { body } = require("express-validator/check");
 
 const Op = db.Sequelize.Op;
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
 
-exports.signup = asyncMiddleware(async (req, res) => {
-  // Save User to Database
-  //signup
-  console.log("Processing func -> SignUp");
-  const user = await User.create({
-    name: req.body.name,
-    username: req.body.username,
-    email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, 8)
-  });
-  const roles = await Role.findAll({
-    where: {
-      name: {
-        [Op.or]: req.body.roles
-      }
+exports.validate = method => {
+  switch (method) {
+    case "signup": {
+      return [
+        body("email", "harus dalam bentuk mail").isEmail(),
+        body("password", "password minimal 6").isLength({ min: 6 })
+      ];
     }
-  });
+  }
+};
+
+exports.signup = asyncMiddleware(async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(404).json({ errors: errors.array() });
+      return;
+    }
+
+    // Save User to Database
+    //signup
+    console.log("Processing func -> SignUp");
+    const user = await User.create({
+      name: req.body.name,
+      username: req.body.username,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8)
+    });
+    const roles = await Role.findAll({
+      where: {
+        name: {
+          [Op.or]: req.body.roles
+        }
+      }
+    });
+    await user.setRoles(roles);
+  } catch (err) {
+    return next(err);
+  }
   const sgMail = require("@sendgrid/mail");
   sgMail.setApiKey(
     "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"
@@ -38,7 +62,7 @@ exports.signup = asyncMiddleware(async (req, res) => {
     html: "<strong>thankyou for register, enjoy with your code</strong>"
   };
   sgMail.send(msg);
-  await user.setRoles(roles);
+
   res.status(201).send({
     status: "User registered successfully!"
   });
--- router/router.js
+++ router/router.js
@@ -14,6 +14,7 @@ module.exports = function(app) {
       verifySignUp.checkDuplicateUserNameOrEmail,
       verifySignUp.checkRolesExisted
     ],
+    authController.validate("signup"),
     authController.signup
   );
   app.post("/login", authController.signin);
